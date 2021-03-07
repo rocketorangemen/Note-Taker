6594f4500c6792ff294a011bcd3fdc795f5920f7
Mon <rocketorangemen@gmail.com>
2021-03-06 23:32:24 -0600

    creating endpoints

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// // parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// // parse incoming JSON data
+// app.use(express.json());
+
+// respond with "hello world" when a GET request is made to the homepage
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/develop/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/develop/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
